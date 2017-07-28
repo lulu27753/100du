6dd5f26dad17ea400ed139e7bee7c45447741940
lulu27753 <lulu27753@163.com>
2017-07-29 00:33:29 +0800

    文字弹性滑动效果JS

--- js/100du.js
+++ js/100du.js
@@ -32,4 +32,59 @@ $(function  () {
 			if ($(this).val() == '') {$(this).val(defautTexts[iNow])};
 		});
 	})();
+	// 文字弹性滑动效果
+	(function updateText () {
+
+		var arrData = [
+			{ 'name':'萱萱', 'time':4, 'title':'那些灿烂华美的瞬间', 'url':'#/' },
+			{ 'name':'畅畅', 'time':5, 'title':'广东3天抓获涉黄疑犯', 'url':'#' },
+			{ 'name':'萱萱', 'time':6, 'title':'国台办回应王郁琦', 'url':'#' },
+			{ 'name':'畅畅', 'time':7, 'title':'那些灿烂华美的瞬间', 'url':'#' },
+			{ 'name':'萱萱', 'time':8, 'title':'那些灿烂华美的瞬间', 'url':'#/' },
+			{ 'name':'畅畅', 'time':9, 'title':'广东3天抓获涉黄疑犯', 'url':'#' },
+			{ 'name':'萱萱', 'time':10, 'title':'国台办回应王郁琦', 'url':'#' },
+			{ 'name':'畅畅', 'time':11, 'title':'那些灿烂华美的瞬间', 'url':'#' }
+		];
+		var oDiv = $('#update');
+		var oUl = oDiv.find('ul');
+		var iH = 0;
+		var str = '';
+		var oScrollUp = $('#search .update .triangle_up');
+		var oScrollDown = $('#search .update .triangle_down');
+		var iNow = 0;
+		var timer = null;
+		// 数据初始化
+		for (var i = arrData.length - 1; i >= 0; i--) {
+			str += '<li><a href="#"><strong>'+arrData[i].name +'</strong><span> '+arrData[i].time +'分钟前 </span>写了一篇新文章：'+arrData[i].title+'</a></li>';
+		};
+		oUl.html(str);
+		// 数据初始化完毕
+		// 向上滚动
+		iH = oUl.find('li').height();
+		function move (i) {
+			iNow += i; 
+			if (Math.abs(iNow) > arrData.length-1) {iNow = 0;};
+			if (iNow > 0) {iNow = -(arrData.length-1);};
+			oUl.animate({top:iH*iNow});
+		}
+		
+		oScrollUp.click(function() {
+			move(-1);
+		});
+		// 向下滚动
+		oScrollDown.click(function() {
+			move(1);
+		});
+		// 自动滚动
+		function autoPlay () {
+			timer = setInterval(function  () {
+				move(-1);
+			},1500)
+		}
+		autoPlay ();
+		// 鼠标移入停止自动滚动，移出时恢复自动滚动
+		oDiv.hover(function  () {
+			clearInterval(timer);
+		},autoPlay);
+	})();
 });
